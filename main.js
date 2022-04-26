20d153a6ddf5479ad0cfb34c2bcc30a413c5dd69
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-04-26 16:26:10 -0400

    added JS Added pseudo code for js skeleton and linked buttons with json. Added classes in css to assign and change the order of the tiles.

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
-    
+    // define variables for the tiles in works and projects for startinmg points 
+    // works 1
+    // works 2
+    // works 3
+    // proj 1
+    // proj 2
+    // proj 3
+
+    $("#workLeft").on('click', () => {
+        console.log('work left')
+        // call shift function with the parametors of works and -1
+    })
+    $("#workRight").on('click', () => {
+        console.log('work right')
+        // call shift function with the parametors of works and 1
+    })
+    $("#projLeft").on('click', () => {
+        console.log('projects left')
+        // call shift function with the parametors of proj and -1
+    })
+    $("#projRight").on('click', () => {
+        console.log('projects right')
+        // call shift function with the parametors of proj and 1
+    })
+
+    // create a function that will shift the tile location based on the (arr, direction)
+    // IF statements filtering the item and direction given 
+    //      add and remove classes to shift the order 
+    // repeat the two lines above until all options are taken
+
+
+
 })
+
